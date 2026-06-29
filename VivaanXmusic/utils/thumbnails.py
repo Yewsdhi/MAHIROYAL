40bd2fc73419cce4eb386a9006a1c3d8e5b39fe9
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-06-29 18:58:30 +0530

    Update thumbnails.py

--- VivaanXmusic/utils/thumbnails.py
+++ VivaanXmusic/utils/thumbnails.py
@@ -51,7 +51,7 @@ def get_contrasting_color(bg_color):
     
     # Return white for dark backgrounds, dark for light backgrounds
     return (255, 255, 255) if luminance < 128 else (50, 50, 50)
-async def get_thumb(videoid,user_id):
+async def get_thumb(videoid):
     final_path = f"cache/{videoid}.png"
     if os.path.isfile(final_path):
         return final_path
