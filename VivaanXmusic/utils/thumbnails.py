008e038150dc72a6b3c5d3826f5505f684468a42
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-06-29 18:24:48 +0530

    Update thumbnails.py

--- VivaanXmusic/utils/thumbnails.py
+++ VivaanXmusic/utils/thumbnails.py
@@ -11,7 +11,7 @@ from PIL import (
     ImageFilter,
     ImageFont,
 )
-from youtubesearchpython.future import Video
+from youtubesearchpython.__future__ import VideosSearch
 from config import YOUTUBE_IMG_URL
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
