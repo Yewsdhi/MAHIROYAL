037de4713af481cfad0df8905f75b9b906dec91f
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-06-30 15:52:52 +0530

    Update stream.py

--- VivaanXmusic/utils/stream/stream.py
+++ VivaanXmusic/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from VivaanXmusic import Carbon, YouTube, app
-from VivaanXmusic.core.call import Siddu
-from VivaanXmusic.misc import db
-from VivaanXmusic.utils.database import add_active_video_chat, is_active_chat
-from VivaanXmusic.utils.exceptions import AssistantErr
-from VivaanXmusic.utils.inline import aq_markup, close_markup, stream_markup
-from VivaanXmusic.utils.pastebin import SidduBin
-from VivaanXmusic.utils.stream.queue import put_queue, put_queue_index
-from VivaanXmusic.utils.thumbnails import get_thumb
+from SHUKLAMUSIC import Carbon, YouTube, app
+from SHUKLAMUSIC.core.call import SHUKLA
+from SHUKLAMUSIC.misc import db
+from SHUKLAMUSIC.utils.database import add_active_video_chat, is_active_chat
+from SHUKLAMUSIC.utils.exceptions import AssistantErr
+from SHUKLAMUSIC.utils.inline import aq_markup, close_markup, stream_markup
+from SHUKLAMUSIC.utils.pastebin import SHUKLABin
+from SHUKLAMUSIC.utils.stream.queue import put_queue, put_queue_index
+from SHUKLAMUSIC.utils.thumbnails import get_thumb
 
 
 async def stream(
@@ -32,7 +32,7 @@ async def stream(
     if not result:
         return
     if forceplay:
-        await Siddu.force_stop_stream(chat_id)
+        await SHUKLA.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -79,7 +79,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                await Siddu.join_call(
+                await SHUKLA.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -98,7 +98,7 @@ async def stream(
                     "video" if video else "audio",
                     forceplay=forceplay,
                 )
-                img = await get_thumb(vidid,user_id)
+                img = await get_thumb(vidid)
                 button = stream_markup(_, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
@@ -116,7 +116,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await SidduBin(msg)
+            link = await SHUKLABin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -165,7 +165,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Siddu.join_call(
+            await SHUKLA.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -184,7 +184,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            img = await get_thumb(vidid,user_id)
+            img = await get_thumb(vidid)
             button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
@@ -225,7 +225,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Siddu.join_call(chat_id, original_chat_id, file_path, video=None)
+            await SHUKLA.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -277,7 +277,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Siddu.join_call(chat_id, original_chat_id, file_path, video=status)
+            await SHUKLA.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -333,7 +333,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await Siddu.join_call(
+            await SHUKLA.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -352,7 +352,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            img = await get_thumb(vidid,user_id)
+            img = await get_thumb(vidid)
             button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Siddu.join_call(
+            await SHUKLA.join_call(
                 chat_id,
                 original_chat_id,
                 link,
