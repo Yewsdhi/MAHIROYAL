b37b3c934315bab9e954cd165e2fd536529b666c
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-07-03 15:16:07 +0530

    Update Youtube.py

--- VivaanXmusic/platforms/Youtube.py
+++ VivaanXmusic/platforms/Youtube.py
@@ -5,7 +5,7 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from py_yt import VideosSearch
+from youtubesearchpython.future import VideosSearch
 import aiohttp
 
 # API_URL and API_KEY
