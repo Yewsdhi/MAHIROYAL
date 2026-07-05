e32052535578a5f2b018e6d93719c210797cb291
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-07-05 14:25:56 +0530

    Update stream.py

--- VivaanXmusic/utils/stream/stream.py
+++ VivaanXmusic/utils/stream/stream.py
@@ -31,11 +31,8 @@ async def stream(
 ):
     if not result:
         return
-
     if forceplay:
-        await Aviax.force_stop_stream(chat_id)
-
-
+        await Siddu.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['play_19']}\n\n"
         count = 0
@@ -52,13 +49,10 @@ async def stream(
                 ) = await YouTube.details(search, False if spotify else True)
             except:
                 continue
-
             if str(duration_min) == "None":
                 continue
-
             if duration_sec > config.DURATION_LIMIT:
                 continue
-
             if await is_active_chat(chat_id):
                 await put_queue(
                     chat_id,
@@ -85,11 +79,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_14"])
-                
-                if not file_path:
-                    raise AssistantErr(_["play_14"])
-
-                await Aviax.join_call(
+                await Siddu.join_call(
                     chat_id,
                     original_chat_id,
                     file_path,
@@ -108,7 +98,7 @@ async def stream(
                     "video" if video else "audio",
                     forceplay=forceplay,
                 )
-                img = await gen_thumb(vidid)
+                img = await get_thumb(vidid,user_id)
                 button = stream_markup(_, chat_id)
                 run = await app.send_photo(
                     original_chat_id,
@@ -123,11 +113,10 @@ async def stream(
                 )
                 db[chat_id][0]["mystic"] = run
                 db[chat_id][0]["markup"] = "stream"
-
         if count == 0:
             return
         else:
-            link = await AviaxBin(msg)
+            link = await SidduBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -141,7 +130,6 @@ async def stream(
                 caption=_["play_21"].format(position, link),
                 reply_markup=upl,
             )
-
     elif streamtype == "youtube":
         link = result["link"]
         vidid = result["vidid"]
@@ -149,22 +137,12 @@ async def stream(
         duration_min = result["duration_min"]
         thumbnail = result["thumb"]
         status = True if video else None
-
-
-        current_queue = db.get(chat_id)
-        if current_queue is not None and len(current_queue) >= 10:
-             return await app.send_message(original_chat_id, "You can't add more than 10 songs to the queue.")
-
         try:
             file_path, direct = await YouTube.download(
                 vidid, mystic, videoid=True, video=status
             )
         except:
             raise AssistantErr(_["play_14"])
-        
-        if not file_path:
-             raise AssistantErr(_["play_14"])
-
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -187,7 +165,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Aviax.join_call(
+            await Siddu.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -206,7 +184,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            img = await gen_thumb(vidid)
+            img = await get_thumb(vidid,user_id)
             button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
@@ -221,15 +199,10 @@ async def stream(
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "stream"
-
     elif streamtype == "soundcloud":
         file_path = result["filepath"]
         title = result["title"]
         duration_min = result["duration_min"]
-        
-        if not file_path:
-            raise AssistantErr(_["play_14"])
-
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -252,7 +225,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Aviax.join_call(chat_id, original_chat_id, file_path, video=None)
+            await Siddu.join_call(chat_id, original_chat_id, file_path, video=None)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -270,23 +243,18 @@ async def stream(
                 original_chat_id,
                 photo=config.SOUNCLOUD_IMG_URL,
                 caption=_["stream_1"].format(
-                    config.SUPPORT_GROUP, title[:23], duration_min, user_name
+                    config.SUPPORT_CHAT, title[:23], duration_min, user_name
                 ),
                 reply_markup=InlineKeyboardMarkup(button),
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
-
     elif streamtype == "telegram":
         file_path = result["path"]
         link = result["link"]
         title = (result["title"]).title()
         duration_min = result["dur"]
         status = True if video else None
-        
-        if not file_path:
-            raise AssistantErr(_["play_5"])
-
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -309,7 +277,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Aviax.join_call(chat_id, original_chat_id, file_path, video=status)
+            await Siddu.join_call(chat_id, original_chat_id, file_path, video=status)
             await put_queue(
                 chat_id,
                 original_chat_id,
@@ -333,8 +301,6 @@ async def stream(
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
-
-
     elif streamtype == "live":
         link = result["link"]
         vidid = result["vidid"]
@@ -342,7 +308,6 @@ async def stream(
         thumbnail = result["thumb"]
         duration_min = "Live Track"
         status = True if video else None
-        
         if await is_active_chat(chat_id):
             await put_queue(
                 chat_id,
@@ -368,12 +333,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            
-
-            if not file_path:
-                raise AssistantErr(_["play_14"])
-
-            await Aviax.join_call(
+            await Siddu.join_call(
                 chat_id,
                 original_chat_id,
                 file_path,
@@ -392,7 +352,7 @@ async def stream(
                 "video" if video else "audio",
                 forceplay=forceplay,
             )
-            img = await gen_thumb(vidid)
+            img = await get_thumb(vidid,user_id)
             button = stream_markup(_, chat_id)
             run = await app.send_photo(
                 original_chat_id,
@@ -407,17 +367,10 @@ async def stream(
             )
             db[chat_id][0]["mystic"] = run
             db[chat_id][0]["markup"] = "tg"
-
-
     elif streamtype == "index":
         link = result
         title = "ɪɴᴅᴇx ᴏʀ ᴍ3ᴜ8 ʟɪɴᴋ"
         duration_min = "00:00"
-        
-
-        if not link:
-             raise AssistantErr(_["play_14"])
-
         if await is_active_chat(chat_id):
             await put_queue_index(
                 chat_id,
@@ -438,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Aviax.join_call(
+            await Siddu.join_call(
                 chat_id,
                 original_chat_id,
                 link,
