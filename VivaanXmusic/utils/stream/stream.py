51e1ba15d182b33dc879ee26ff3c97c524e6d35d
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-07-05 14:06:26 +0530

    Update stream.py

--- VivaanXmusic/utils/stream/stream.py
+++ VivaanXmusic/utils/stream/stream.py
@@ -5,15 +5,15 @@ from typing import Union
 from pyrogram.types import InlineKeyboardMarkup
 
 import config
-from AviaxMusic import Carbon, YouTube, app
-from AviaxMusic.core.call import Aviax
-from AviaxMusic.misc import db
-from AviaxMusic.utils.database import add_active_video_chat, is_active_chat
-from AviaxMusic.utils.exceptions import AssistantErr
-from AviaxMusic.utils.inline import aq_markup, close_markup, stream_markup
-from AviaxMusic.utils.pastebin import AviaxBin
-from AviaxMusic.utils.stream.queue import put_queue, put_queue_index
-from AviaxMusic.utils.thumbnails import gen_thumb
+from VivaanXmusic import Carbon, YouTube, app
+from VivaanXmusic.core.call import Siddu
+from VivaanXmusic.misc import db
+from VivaanXmusic.utils.database import add_active_video_chat, is_active_chat
+from VivaanXmusic.utils.exceptions import AssistantErr
+from VivaanXmusic.utils.inline import aq_markup, close_markup, stream_markup
+from VivaanXmusic.utils.pastebin import AviaxBin
+from VivaanXmusic.utils.stream.queue import put_queue, put_queue_index
+from VivaanXmusic.utils.thumbnails import gen_thumb
 
 
 async def stream(
