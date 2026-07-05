914c7fdf1a1839f62207d29db20575b424fbaee8
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-07-05 14:20:11 +0530

    Update play.py

--- VivaanXmusic/plugins/play/play.py
+++ VivaanXmusic/plugins/play/play.py
@@ -7,22 +7,22 @@ from pyrogram.types import InlineKeyboardMarkup, InputMediaPhoto, Message
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from AviaxMusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
-from AviaxMusic.core.call import Aviax
-from AviaxMusic.utils import seconds_to_min, time_to_seconds
-from AviaxMusic.utils.channelplay import get_channeplayCB
-from AviaxMusic.utils.decorators.language import languageCB
-from AviaxMusic.utils.decorators.play import PlayWrapper
-from AviaxMusic.utils.formatters import formats
-from AviaxMusic.utils.inline import (
+from VivaanXmusic import Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app
+from VivaanXmusic.core.call import Siddu
+from VivaanXmusic.utils import seconds_to_min, time_to_seconds
+from VivaanXmusic.utils.channelplay import get_channeplayCB
+from VivaanXmusic.utils.decorators.language import languageCB
+from VivaanXmusic.utils.decorators.play import PlayWrapper
+from VivaanXmusic.utils.formatters import formats
+from VivaanXmusic.utils.inline import (
     botplaylist_markup,
     livestream_markup,
     playlist_markup,
     slider_markup,
     track_markup,
 )
-from AviaxMusic.utils.logger import play_logs
-from AviaxMusic.utils.stream.stream import stream
+from VivaanXmusic.utils.logger import play_logs
+from VivaanXmusic.utils.stream.stream import stream
 from config import BANNED_USERS, lyrical
 
 SHELL_INJECTION_PATTERNS = [
