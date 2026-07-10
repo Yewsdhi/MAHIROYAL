7bbbf9ed5f45d2e2955cb2a7dd6a6e4949d7b496
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-07-10 16:01:24 +0530

    Update sticker response in start.py

--- VivaanXmusic/plugins/bot/start.py
+++ VivaanXmusic/plugins/bot/start.py
@@ -34,7 +34,7 @@ async def start_pm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("CAACAgUAAx0CdQO5IgACMTplUFOpwDjf-UC7pqVt9uG659qxWQACfQkAAghYGFVtSkRZ5FZQXDME")
+            await message.reply_sticker("CAACAgUAAxkBAAFOuWNqUMj28Rrfpr3APoGQH5oC2qGq7wACwyAAAh7UeVa0AcYt3xU9tzwE")
             return await message.reply_photo(
                 photo=random.choice(config.START_IMG_URL),
                 caption=_["help_1"].format(config.SUPPORT_CHAT),
