0736d0310549e5ae545e72eb4250f5d2b269e958
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-07-15 14:31:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/about_Maahi_XD")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ll_DPZ_WORLDS_lll")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 ASSISTANT_LEAVE_TIME = int(getenv("ASSISTANT_LEAVE_TIME",  18000))
 
 
