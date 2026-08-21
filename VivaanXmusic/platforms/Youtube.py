ec24b7cee7e02988f30705900be978f6eea04e00
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-08-21 20:53:34 +0530

    Add files via upload

--- VivaanXmusic/platforms/Youtube.py
+++ VivaanXmusic/platforms/Youtube.py
@@ -13,10 +13,10 @@ from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
-from youtubesearchpython.future import VideosSearch
-from VivaanXmusic import LOGGER
-from VivaanXmusic.utils.database import is_on_off
-from VivaanXmusic.utils.formatters import time_to_seconds
+from ytSearch import VideosSearch, Playlist
+from AnonXMusic import LOGGER
+from AnonXMusic.utils.database import is_on_off
+from AnonXMusic.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 
 logger = LOGGER(__name__)
